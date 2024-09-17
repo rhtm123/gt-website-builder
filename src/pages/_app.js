d00b41145ade4aa63b3dd864244167fd8712c68c
rhtm123 <maurya.iitk@gmail.com>
2024-09-17 11:12:52 +0530

    google tag added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,11 +12,27 @@ import NextNProgress from 'nextjs-progressbar';
 
 // import Footer from "@/components/Footer";
 
+import { useEffect } from 'react';
+import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YSWYGRTZ73');
+  }, []);
+
+
   return (
     <>
+    <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-YSWYGRTZ73"
+      />
+      
     <ThemeProvider
     enableSystem = {false}
     
